c0e651f66299106f814d9b0a53e83640f62b1d8b
yufei.shao <yufei.shao@de.bosch.com>
2020-01-21 23:21:54 +0100

    First Commit

---
+++ Assets/cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube : MonoBehaviour
+{
+
+    //Vector3 temposition;
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  print("I am a cube!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.Translate(1f * Time.deltaTime, 0f, 0f);
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f);
+        //transform.position = Vector3.MoveTowards(transform.position, position1.transform.position, 0);
+
+        //temposition = transform.position;
+        //temposition.x += 1f;
+        //transform.position = temposition;
+
+        //transform.position += new Vector3(0.1f, 0f, 0f);
+
+        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(0f, 0f, 1f), 1.0f * Time.deltaTime);
+
+        transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("Building_01_01").transform.position, 1.0f * Time.deltaTime);
+      
+
+
+
+        // Move the object forward along its z axis 1 unit/second.
+       // transform.Translate(0, 0, Time.deltaTime);
+
+        // Move the object upward in world space 1 unit/second.
+      //  transform.Translate(0, Time.deltaTime, 0, Space.World);
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour
+{
+    public Transform[] target;
+    public float speed;
+    public int current;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+     //  if (transform.position != target[current].position) {
+        //   Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
+        // Rigidbody rb = GetComponent<Rigidbody>();
+         //  GetComponent<Rigidbody>().MovePosition(pos);
+        // }
+      //   else current = (current + 1) % target.Length;
+        for (int i= 0;i <target.Length; i++){
+            if (transform.position != target[current].position)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, target[i].position, speed * Time.deltaTime);
+                //transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("box").transform.position, 1.0f * Time.deltaTime);
+            }
+
+        }
+
+    }
+}
